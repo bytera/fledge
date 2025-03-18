65e7430db65a3dbb39688d4ca5babf97c5e21b8f
Eitetsu Terakado <23092186+bytera@users.noreply.github.com>
2025-03-18 23:41:35 +0900

    Enable counting tasks

--- mdn/moz-todo-react/src/App.jsx
+++ mdn/moz-todo-react/src/App.jsx
@@ -20,6 +20,9 @@ function App(props) {
       key={task.id}
     />
   ));
+  const tasksNoun = taskList.length != 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -29,7 +32,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
