d375d74e2c901eda4fb22770b2dea825baed3f8e
Eitetsu Terakado <23092186+bytera@users.noreply.github.com>
2025-03-19 00:19:38 +0900

    Functions for completing and deleting tasks

--- mdn/moz-todo-react/src/App.jsx
+++ mdn/moz-todo-react/src/App.jsx
@@ -1,26 +1,46 @@
+import { useState } from "react";
 import { nanoid } from "nanoid";
 import Todo from "./components/Todo";
-import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
-import { useState } from "react";
+import FilterButton from "./components/FilterButton";
 
 function App(props) {
-  const [tasks, setTasks] = useState(props.tasks);
-
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
   }
 
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const [tasks, setTasks] = useState(props.tasks);
   const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
-  const tasksNoun = taskList.length != 1 ? "tasks" : "task";
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
